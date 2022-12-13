062fdcc8e849d8afaac13b0575b831f685d3f11e
umiklsum <umikulsum06809@gmail.com>
2022-12-13 08:38:48 +0700

    unggah ke github

---
+++ application/views/v_dashboard.php
@@ -0,0 +1,9 @@
+<div class="container-fluid mt-4">
+<div class="jumbotron">
+        <h1 class="display-4">KLINIK UMI KULSUM</h1>
+        <p class="lead">Aplikasi hasil latihan framework Codeigniter.</p>
+        <hr class="my-4">
+        <a class="btn btn-primary btn-lg" href="https://www.youtube.com/c/UmiKulsum" role="button">Subscribe</a>
+    </div>
+</div>
+
